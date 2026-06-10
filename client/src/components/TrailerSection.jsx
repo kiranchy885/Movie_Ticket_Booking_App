e4c65efc43c613b0755c6347e69e496452fc571e
kiranchy885 <kiranc2056@gmail.com>
2026-06-10 18:54:25 +0545

    Updated TrailerSection component

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { dummyTrailers } from '../assets/assets'
 import BlurCircle from './BlurCircle'
+import { PlayCircleIcon } from 'lucide-react'
 
 
 const TrailerSection = () => {
@@ -12,9 +13,26 @@ const TrailerSection = () => {
             mx-auto'>Trailers</p>
 
         <div className='relative mt-6'>
-            <BlurCircle />
+            <BlurCircle top='-100px' right='-100px' />
             <ReactPlayer url={currentTrailer.videoUrl} controls={false}
             className="mx-auto max-w-full" width="960px" height="540px"/>
+        </div>
+        
+        <div className='group grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
+            {dummyTrailers.map((trailer)=>(
+                <div key={trailer.image} className='relative group-hover:not-hover:opacity-50
+                hover:-translate-y-1 duration-300 transition max-md:h-60 md:max-h-60
+                cursor-pointer' onClick={()=> setCurrentTrailer
+                (trailer)}>
+                    
+                    <img src={trailer.image} alt="tailer" className='rounded-lg w-full
+                    h-full object-cover brightness-75'/>
+                    <PlayCircleIcon strokeWidth={1.6} className="absolute top-1/2
+                    left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2
+                    -translate-y-1/2"/>
+                </div>
+            ))}
+
         </div>
         </div>
     )

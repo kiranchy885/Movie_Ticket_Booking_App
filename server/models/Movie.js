32922500fe536526ef57fbb5a9f5c9a6665a2942
kiranchy885 <kiranc2056@gmail.com>
2026-09-03 23:59:05 +0545

    Add Movie

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,22 +1,76 @@
 import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema(
-    {
-        _id: {type: String, required: true},
-        title: {type: String, required: true},
-        overview: {type: String, required: true},
-        poster_path: {type: String, required: true},
-        backdrop_path: {type: String, required: true},
-        release_date: {type: String, required: true},
-        original_language: {type: String},
-        tagline: {type: String},
-        genres: {type: Array, required: true},
-        casts: {type: Array, required: true},
-        vote_average: {type: Number, required: true},
-        runtime: {type: Number, required: true},
-    }, {timestamps: true}
-)
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
 
-const Movie = mongoose.model('Movie', movieSchema)
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    overview: {
+      type: String,
+      default: "",
+    },
+
+    poster_path: {
+      type: String,
+      default: "",
+    },
+
+    backdrop_path: {
+      type: String,
+      default: "",
+    },
+
+    release_date: {
+      type: String,
+      default: "",
+    },
+
+    original_language: {
+      type: String,
+      default: "",
+    },
+
+    tagline: {
+      type: String,
+      default: "",
+    },
+
+    genres: {
+      type: Array,
+      default: [],
+    },
+
+    casts: {
+      type: Array,
+      default: [],
+    },
+
+    vote_average: {
+      type: Number,
+      default: 0,
+    },
+
+    runtime: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Movie = mongoose.model(
+  "Movie",
+  movieSchema
+);
 
 export default Movie;

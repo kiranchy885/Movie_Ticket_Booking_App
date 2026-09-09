9d76bf48bd7c355c9f44578ad377d2e832b9bb11
kiranchy885 <kiranc2056@gmail.com>
2026-09-09 17:03:23 +0545

    Add Show

--- server/models/Show.js
+++ server/models/Show.js
@@ -2,17 +2,75 @@ import mongoose from "mongoose";
 
 const showSchema = new mongoose.Schema(
     {
-       movie: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Movie",
-    required: true,
-},
-        showDateTime: { type: Date, required: true},
-        showPrice: { type: Number, required: true},
-        occupiedSeats: { type: Object, default: {} },
+        movie: {
+            type: String,
+            ref: "Movie",
+            required: true,
+        },
 
-    }, { minimize: false}
-)
-const Show = mongoose.model("Show", showSchema)
+        // ==========================================
+        // THEATER
+        // ==========================================
+        theaterId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Theater",
+            required: true,
+        },
+
+        theaterName: {
+            type: String,
+            default: "",
+        },
+
+        theaterLat: {
+            type: Number,
+            default: 0,
+        },
+
+        theaterLng: {
+            type: Number,
+            default: 0,
+        },
+
+        theaterCity: {
+            type: String,
+            default: "",
+        },
+
+        theaterAddress: {
+            type: String,
+            default: "",
+        },
+
+        // ==========================================
+        // SHOW DATE/TIME
+        // ==========================================
+        showDateTime: {
+            type: Date,
+            required: true,
+        },
+
+        // ==========================================
+        // PRICE
+        // ==========================================
+        showPrice: {
+            type: Number,
+            required: true,
+        },
+
+        // ==========================================
+        // OCCUPIED SEATS
+        // ==========================================
+        occupiedSeats: {
+            type: Object,
+            default: {},
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Show = mongoose.model("Show", showSchema);
 
 export default Show;
